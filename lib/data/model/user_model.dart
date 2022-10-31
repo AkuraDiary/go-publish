6cd33a9558dbaba4df3bfbf1ed6ccbfb8ae6101e
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-10-31 11:09:02 +0700

    added user model

---
+++ lib/data/model/user_model.dart
@@ -0,0 +1,24 @@
+class user{
+  String? name;
+  String? email;
+  String? password;
+  String? role;
+
+  user({required this.name, required this.email, required this.password, required this.role});
+
+  user.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    email = json['email'];
+    password = json['password'];
+    role = json['role'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.name;
+    data['email'] = this.email;
+    data['password'] = this.password;
+    data['role'] = this.role;
+    return data;
+  }
+}
